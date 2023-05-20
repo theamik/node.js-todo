4e93f26a9ac8ac6d34f8dca4d82d42640ba1e92c
theamik <amik.sylhet@gmail.com>
2023-05-20 12:58:31 +0600

    engine update

--- data/database.js
+++ data/database.js
@@ -3,8 +3,8 @@ import mongoose from "mongoose";
 export const conectDB = ()=>{
     mongoose.connect(process.env.MONGO_URI,{
     dbName:"backendApi",
-}).then(()=> {
-    console.log('connected to MongoDB')
+}).then((c)=> {
+    console.log(`connected to MongoDB with ${c.connection.host}`)
 
 }).catch((error)=>{
     console.log(error)
